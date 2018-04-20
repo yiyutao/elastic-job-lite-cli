3d07173572c3b6fca087c5c86d832cce760b7f94
yiyutao <296328516@qq.com>
2018-04-20 14:23:19 +0800

    springboot框架搭建

---
+++ src/main/java/net/yiyutao/elasticjoblitecli/ElasticJobLiteCliApplication.java
@@ -0,0 +1,15 @@
+package net.yiyutao.elasticjoblitecli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author masterYI
+ */
+@SpringBootApplication
+public class ElasticJobLiteCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticJobLiteCliApplication.class, args);
+    }
+}
